e1df5b06997115a80100107fe47c5d6d72f48206
Кынтэрец Андрей <kynterets@gmail.com>
2021-05-28 10:25:30 +0400

    Старт

---
+++ ContactList/Models/Contact.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace ContactList.Models
+{
+    public class Contact
+    {
+        public int Id { get; set; }
+        public string surname { get; set; }
+        public string name { get; set; }
+        public string patronymic { get; set; }
+
+      [DataType(DataType.Date)]
+        public DateTime birthday { get; set; }
+        public string organization { get; set; }
+        public string position { get; set; }
+    }
+}
