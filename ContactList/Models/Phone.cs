e93189b6e184e35acf9970541850b706a8614e29
Andrey <kynterets@gmail.com>
2021-05-30 09:17:47 +0400

    Сложный объект

---
+++ ContactList/Models/Phone.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+namespace ContactList.Models
+{
+    public class Phone
+    {
+        public int? PhoneId { get; set; }
+        public int ContactId { get; set; }
+
+        [DataType(DataType.PhoneNumber)]
+        [StringLength(12)]
+        [Display(Name = "Телефон")]
+        public string PhoneNumber { get; set; }
+
+        public Contact Contact { get; set; }
+    }
+}
